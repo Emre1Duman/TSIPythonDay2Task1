24371924381a739f039a29ee4ae8f46db298b91e
unknown <emred2010@hotmail.com>
2022-04-21 10:39:50 +0100

    Day2Task1

---
+++ Task1.py
@@ -0,0 +1,36 @@
+'''
+pairs = []
+
+UserInput = input("Enter list of pairs ")
+
+UserPairs = UserInput.split()
+
+print("pairs ", UserInput)
+
+for i in range(len(USerPairs)):
+    UserPairs[i] = int(UserPairs[i])
+
+
+if
+'''
+#ExampleInput = [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+
+    #if ExampleInput =
+
+
+
+
+UserInput = int(input("Enter your age "))
+
+if UserInput >= 55:
+    print("You are qualified for a Senior membership ")
+else:
+    print("You are qualified for an Open membership ")
+    
+
+
+
+
+
+
+
